f287a2db7a587652829a78f6d060c845aadfecc1
Luis-A-Dik <lalbertodik@gmail.com>
2024-06-14 19:30:46 -0500

    Add files via upload

---
+++ Analizador de texto/index.js
@@ -0,0 +1,8 @@
+// este es un comentario de una sola linea
+/*este es un comentario 
+de varias lineas */
+const valorString = "123.45";
+const valorNumerico = parseInt(valorString);
+console.log(valorNumerico); // Salida: 123
+
+
